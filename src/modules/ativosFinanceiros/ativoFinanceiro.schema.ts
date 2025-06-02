50d1f72ad3272d1d9dad654e1c9038f23f8c0f87
deiz0n <eduardomacedosilva@outlook.com>
2025-06-02 19:10:36 -0300

    feat: adição do schema para criação de ativo financeiro com validações de nome e valor atual

--- src/modules/ativosFinanceiros/ativoFinanceiro.schema.ts
+++ src/modules/ativosFinanceiros/ativoFinanceiro.schema.ts
@@ -1,5 +1,14 @@
 import { z } from 'zod';
 
+export const criarAtivoFinanceiroSchema = z.object({
+    nome: z.string().nonempty(
+        { message: "Nome é obrigatório" }
+    ),
+    valorAtual: z.number().nonnegative(
+        { message: "O valor do ativo precisa ser positivo" }
+    )
+})
+
 export const ativoFinanceiroResponseShema = z.object({
     id: z.string().uuid(),
     nome: z.string().nonempty(
