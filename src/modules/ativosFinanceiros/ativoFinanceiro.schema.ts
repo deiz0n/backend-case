c1c9389baa2d04d08e7acdcada20f2418d25a980
deiz0n <eduardomacedosilva@outlook.com>
2025-06-02 19:08:20 -0300

    feat: renomeação do schema de ativos para ativoFinanceiro e adição da validação de UUID para id

--- src/modules/ativosFinanceiros/ativoFinanceiro.schema.ts
+++ src/modules/ativosFinanceiros/ativoFinanceiro.schema.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 export const ativoFinanceiroResponseShema = z.object({
+    id: z.string().uuid(),
     nome: z.string().nonempty(
         { message: "Nome é obrigatório" }
     ),
