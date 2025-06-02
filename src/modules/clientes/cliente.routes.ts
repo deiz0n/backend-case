503f7129da20337361cbc7866bc75d0500f07067
deiz0n <eduardomacedosilva@outlook.com>
2025-06-02 09:55:20 -0300

    feat: implementação do endpoint buscarTodos

--- src/modules/clientes/cliente.routes.ts
+++ src/modules/clientes/cliente.routes.ts
@@ -8,6 +8,21 @@ import {responseSchemaErro, responseSchemaSucesso} from "../../core/schemas/resp
 export class ClienteRoutes {
     constructor(private clienteController: ClienteController) {}
 
+    async buscarTodos(fastify: FastifyInstance) {
+        fastify.get(
+            "/",
+            {
+                schema: {
+                    response: {
+                        200: zodToJsonSchema(responseSchemaSucesso),
+                        500: zodToJsonSchema(responseSchemaErro),
+                    },
+                },
+            },
+            (request, reply) => this.clienteController.buscarTodosHanlder(request, reply)
+        );
+    }
+
     async criarCliente(fastify: FastifyInstance) {
         fastify.post(
             "/criar",
