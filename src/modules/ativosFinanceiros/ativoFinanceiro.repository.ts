80596b186dc795b6bbcca546dcab12173b686fa0
deiz0n <eduardomacedosilva@outlook.com>
2025-06-02 19:20:58 -0300

    feat: adição do método para criar ativo financeiro no repositório

--- src/modules/ativosFinanceiros/ativoFinanceiro.repository.ts
+++ src/modules/ativosFinanceiros/ativoFinanceiro.repository.ts
@@ -1,7 +1,14 @@
 import { prisma } from "../../prisma/client";
+import {CriarAtivoFinanceiroInput} from "./ativoFinanceiro.schema";
 
 export class AtivoFinanceiroRepository {
     async buscarTodos() {
         return prisma.ativoFinanceiro.findMany();
+    }
+
+    async criar(data: CriarAtivoFinanceiroInput) {
+        return prisma.ativoFinanceiro.create({
+            data: data
+        });
     }
 }
