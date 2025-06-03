ad5b26bbff2400910cf9af3b528d4a5674cf4915
deiz0n <eduardomacedosilva@outlook.com>
2025-06-02 23:15:50 -0300

    feat: registro das rotas buscarTodos e criar ativos financeiros no módulo principal

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -4,11 +4,21 @@ import { ClienteService } from "./clientes/cliente.service";
 import { ClienteController } from "./clientes/cliente.controller";
 import { ClienteRoutes } from "./clientes/cliente.routes";
 
+import { AtivoFinanceiroRepository } from "./ativosFinanceiros/ativoFinanceiro.repository";
+import { AtivoFinanceiroController } from "./ativosFinanceiros/ativoFinanceiro.controller";
+import { AtivoFinanceiroService } from "./ativosFinanceiros/ativoFinanceiro.service";
+import { AtivoFinanceiroRoutes } from "./ativosFinanceiros/ativoFinanceiro.routes";
+
 export class ModuloPrincipal {
     private clienteRepository = new ClienteRepository();
     private clienteService = new ClienteService(this.clienteRepository);
     private clienteController = new ClienteController(this.clienteService);
     private clienteRoutes = new ClienteRoutes(this.clienteController);
+
+    private ativoFinanceiroRepository = new AtivoFinanceiroRepository();
+    private ativoFinanceiroService = new AtivoFinanceiroService(this.ativoFinanceiroRepository);
+    private ativoFinanceiroController = new AtivoFinanceiroController(this.ativoFinanceiroService);
+    private ativoFinanceiroRoutes = new AtivoFinanceiroRoutes(this.ativoFinanceiroController);
 
     async registrar(fastify: FastifyInstance) {
         await fastify.register(async (instance) => {
@@ -16,5 +26,10 @@ export class ModuloPrincipal {
             await this.clienteRoutes.buscarTodos(instance);
             await this.clienteRoutes.atualizarCliente(instance);
         }, { prefix: "/clientes" });
+
+        await fastify.register(async (instance) => {
+            await this.ativoFinanceiroRoutes.criarAtivoFinanceiro(instance);
+            await this.ativoFinanceiroRoutes.buscarTodos(instance);
+        }, { prefix: "/ativos-financeiros" });
     }
 }
