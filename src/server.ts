4c210ab940f32d99447d49ffa9547f4e0ec64635
deiz0n <eduardomacedosilva@outlook.com>
2025-06-03 12:58:00 -0300

    config: configuração do cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { buildServer } from "./app";
+import cors from "@fastify/cors";
 
 async function start() {
   const server = await buildServer();
+  await server.register(cors, { origin: true })
   await server.listen({ port: 3000, host: '0.0.0.0' });
 }
 
