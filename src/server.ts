0c6793e7dd47a8b8fa30de2061a6ad97a4524ae8
deiz0n <eduardomacedosilva@outlook.com>
2025-06-05 14:57:47 -0300

    feat: adição de suporte a métodos GET, POST e PATCH no CORS

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,10 @@ import cors from "@fastify/cors";
 
 async function start() {
   const server = await buildServer();
-  await server.register(cors, { origin: true })
+  await server.register(cors, {
+    origin: true ,
+    methods: ['GET', 'POST', 'PATCH']
+  });
   await server.listen({ port: 3001, host: '0.0.0.0' });
 }
 
